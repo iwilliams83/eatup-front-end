a2f1df3e4ae84bb92d9402f2a153cda0576b9d91
Indira Williams <indirajamurath@gmail.com>
2018-08-23 09:46:50 -0400

    got heart to toggle between full and outline

--- src/Results/Result.js
+++ src/Results/Result.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Card, Image, Icon } from 'semantic-ui-react'
 
-const Result = props => {
-  const { name, location, phone } = props.result
+export default class Result extends Component{
+  state = { liked: false }
+  // I think this item needs state in order to toggle liked / not liked
+  toggleLiked = () => {
+    const { name, location, phone } = this.props.result
+    this.setState(prevState => {
+      return {liked: !prevState.liked}
+    })
+    this.props.addFavorite(this.props.result['yelp-id'], name, this.props.result['image-url'], location, phone)
+  }
+  render(){
+    const { name, location, phone } = this.props.result
+    return (
+          <Card>
+        <Image src={this.props.result['image-url']} size='medium' />
+          <Card.Content>
+            <Card.Header>
+              {name}
+            </Card.Header>
+            <Card.Description>
+              {location}
+              <br/>
+              {phone}
+            </Card.Description><br/>
+            <Icon onClick={this.toggleLiked} name={this.state.liked ? "heart" : "heart outline"} color='orange'/>
+          </Card.Content>
+      </Card>
+    )
+  }
 
-
-  return (
-        <Card>
-      <Image src={props.result['image-url']} size='medium' />
-        <Card.Content>
-          <Card.Header>
-            {name}
-          </Card.Header>
-          <Card.Description>
-            {location}
-            <br/>
-            {phone}
-          </Card.Description><br/>
-          <Icon onClick={() => props.addFavorite(props.result['yelp-id'], name, props.result['image-url'], location, phone)}
-            name="heart outline" color='orange'/>
-        </Card.Content>
-    </Card>
-  )
 }
 
-export default Result
+//export default Result
